5a5899c5fef879f763d90b382838cf067c34390d
IamSyaiful <sabiteam23@gmail.com>
2026-03-06 22:31:36 +0700

    Update Final

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -8,7 +8,7 @@ class StoreCategoryRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true; // Ubah ke true agar request diizinkan
+        return true; 
     }
 
     public function rules(): array
